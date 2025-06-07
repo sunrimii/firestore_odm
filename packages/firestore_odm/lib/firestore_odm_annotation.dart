b683b92964b0ccedb2cece463d83033029026f0a
shtse8 <shtse8@gmail.com>
2025-06-07 08:39:06 +0100

    chore: clean up duplicate annotation files and add @Target annotations

--- packages/firestore_odm/lib/firestore_odm_annotation.dart
+++
@@ -1,9 +0,0 @@
-/// Annotations for Firestore ODM code generation
-library firestore_odm_annotation;
-
-export 'src/annotations.dart';
-export 'src/firestore_collection.dart';
-export 'src/firestore_document.dart';
-export 'src/firestore_odm.dart';
-export 'src/firestore_query.dart';
-export 'package:cloud_firestore/cloud_firestore.dart';
--- packages/firestore_odm_annotation/lib/src/annotations.dart
+++ packages/firestore_odm_annotation/lib/src/annotations.dart
@@ -1,6 +1,8 @@
 import 'package:meta/meta.dart';
+import 'package:meta/meta_meta.dart';
 
 /// Annotation to mark a class as a Firestore collection
+@Target({TargetKind.classType})
 @immutable
 class CollectionPath {
   /// The path to the Firestore collection
@@ -11,6 +13,7 @@ class CollectionPath {
 }
 
 /// Annotation to mark a field as a subcollection
+@Target({TargetKind.classType})
 @immutable
 class SubcollectionPath {
   /// The path to the subcollection
