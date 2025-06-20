a6226eaf78283b7f9c1c3d1f44e7dc16f9df5f10
shtse8 <shtse8@gmail.com>
2025-06-20 12:13:56 +0100

    feat: implement FirestoreConverter interface and add DateTimeConverter and DurationConverter classes

--- packages/firestore_odm/lib/src/model_converter.dart
+++ packages/firestore_odm/lib/src/model_converter.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 typedef JsonDeserializer<T> = T Function(Map<String, dynamic>);
 typedef JsonSerializer<T> = Map<String, dynamic> Function(T);
 
@@ -5,4 +7,29 @@ class ModelConverter<T> {
   final JsonDeserializer<T> fromJson;
   final JsonSerializer<T> toJson;
   const ModelConverter({required this.fromJson, required this.toJson});
+}
+
+
+
+abstract interface class FirestoreConverter<T, F> {
+  T fromFirestore(F data);
+  F toFirestore(T data);
+}
+
+
+class DateTimeConverter implements FirestoreConverter<DateTime, Timestamp> {
+  @override
+  DateTime fromFirestore(Timestamp data) => data.toDate();
+
+  @override
+  Timestamp toFirestore(DateTime data) => Timestamp.fromDate(data);
+}
+
+
+class DurationConverter implements FirestoreConverter<Duration, int> {
+  @override
+  Duration fromFirestore(int data) => Duration(milliseconds: data);
+
+  @override
+  int toFirestore(Duration data) => data.inMilliseconds;
 }
