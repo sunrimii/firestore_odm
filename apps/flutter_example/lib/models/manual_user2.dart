4811b8f123cec17e8e191ebac3a2b991260c3a44
shtse8 <shtse8@gmail.com>
2025-06-23 16:49:01 +0100

    feat(manual_user2): add ManualUser2 model with EquatableMixin support

---
+++ apps/flutter_example/lib/models/manual_user2.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+import 'package:firestore_odm/firestore_odm.dart';
+
+class ManualUser2 with EquatableMixin {
+  @DocumentIdField()
+  final String id;
+
+  final String name;
+  final String email;
+  final int age;
+  final bool isPremium;
+  final double rating;
+  final List<String> tags;
+  final Map<String, String> preferences;
+
+  const ManualUser2({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.age,
+    this.isPremium = false,
+    this.rating = 0.0,
+    this.tags = const [],
+    this.preferences = const {},
+  });
+
+  @override
+  List<Object?> get props => [id, name, email, age, isPremium, rating, tags, preferences];
+}
