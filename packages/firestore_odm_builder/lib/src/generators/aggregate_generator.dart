4359e149e7deb1634f6080042804e6f6007d6acd
shtse8 <shtse8@gmail.com>
2025-06-24 04:35:23 +0100

    refactor(aggregate_generator): enhance method generation for aggregate field selectors and improve type handling

--- packages/firestore_odm_builder/lib/src/generators/aggregate_generator.dart
+++ packages/firestore_odm_builder/lib/src/generators/aggregate_generator.dart
@@ -1,60 +1,105 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/element/type.dart';
+import 'package:code_builder/code_builder.dart';
 import 'package:firestore_odm_builder/src/utils/nameUtil.dart';
-
 import '../utils/type_analyzer.dart';
 import '../utils/model_analyzer.dart';
 
-/// Generator for aggregate field selectors
+/// Generator for aggregate field selectors using code_builder
 class AggregateGenerator {
-  /// Generate the aggregate field selector extension using ModelAnalysis
-  static void generateAggregateFieldSelectorFromAnalysis(
-    StringBuffer buffer,
+  /// Generate numeric field accessor method for aggregation
+  static Method _generateNumericFieldAccessor(FieldInfo field) {
+    return Method(
+      (b) => b
+        ..docs.add('/// ${field.parameterName} field for aggregation')
+        ..type = MethodType.getter
+        ..name = field.parameterName
+        ..lambda = true
+        ..returns = TypeReference(
+          (b) => b
+            ..symbol = 'AggregateField'
+            ..types.add(field.dartType.reference),
+        )
+        ..body = refer('AggregateField').newInstance([], {
+          'name': literalString(field.jsonFieldName),
+          'parent': refer('this'),
+        }).code,
+    );
+  }
+
+  /// Generate nested custom type accessor for aggregate field selector
+  static Method _generateNestedCustomTypeAccessor(FieldInfo field) {
+    return Method(
+      (b) => b
+        ..docs.add('/// Access nested ${field.parameterName} aggregate fields')
+        ..type = MethodType.getter
+        ..name = field.parameterName
+        ..lambda = true
+        ..returns = TypeReference(
+          (b) => b
+            ..symbol = 'AggregateFieldSelector'
+            ..types.add(field.dartType.reference),
+        )
+        ..body = refer('AggregateFieldSelector').newInstance([], {
+          'name': literalString(field.jsonFieldName),
+          'parent': refer('this'),
+        }).code,
+    );
+  }
+
+  /// Generate aggregate field selector extension using ModelAnalysis
+  static Extension generateAggregateFieldSelectorFromAnalysis(
     ModelAnalysis analysis,
   ) {
-    buffer.writeln(
-      'extension ${NameUtil.getName(analysis.dartType, postfix: 'AggregateFieldSelectorExtension')} on AggregateFieldSelector<${NameUtil.getName(analysis.dartType)}> {',
-    );
-    buffer.writeln('');
+    final className = analysis.dartType.element?.name;
+    if (className == null) {
+      throw ArgumentError('ModelAnalysis must have a valid Dart type element.');
+    }
 
-    // Generate field accessors for all numeric fields from analysis
+    final typeParameters = analysis.typeParameters;
+    final typeParameterNames = typeParameters.map((ref) => ref.symbol).toList();
+    final classNameWithTypeParams = analysis.isGeneric ? '$className<${typeParameterNames.join(', ')}>' : className;
+
+    // Create the target type (AggregateFieldSelector<ClassName<T>>)
+    final targetType = TypeReference(
+      (b) => b
+        ..symbol = 'AggregateFieldSelector'
+        ..types.add(
+          TypeReference(
+            (b) => b
+              ..symbol = className
+              ..types.addAll(typeParameters),
+          ),
+        ),
+    );
+
+    // Generate methods for all aggregatable fields
+    final methods = <Method>[];
     for (final field in analysis.fields.values) {
       // Skip document ID field
       if (field.parameterName == analysis.documentIdFieldName) continue;
 
-      if (TypeAnalyzer.isNumericType(field.dartType)) {
-        _generateNumericFieldAccessor(buffer, field);
-      } else if (TypeAnalyzer.isCustomClass(field.dartType)) {
-        // Generate nested custom type accessor
-        _generateNestedCustomTypeAccessor(buffer, field);
+      final fieldType = field.dartType;
+
+      if (TypeAnalyzer.isNumericType(fieldType)) {
+        // Numeric field for aggregation
+        methods.add(_generateNumericFieldAccessor(field));
+      } else if (TypeAnalyzer.isCustomClass(fieldType)) {
+        // Nested custom class field
+        methods.add(_generateNestedCustomTypeAccessor(field));
       }
     }
 
-    buffer.writeln('}');
-    buffer.writeln('');
-  }
-
-  /// Generate numeric field accessor for aggregation
-  static void _generateNumericFieldAccessor(
-    StringBuffer buffer,
-    FieldInfo field,
-  ) {
-    buffer.writeln('  /// ${field.parameterName} field for aggregation');
-    buffer.writeln(
-      '  AggregateField<${field.dartType}> get ${field.parameterName} => AggregateField(name: \'${field.jsonFieldName}\', parent: this);',
+    // Create extension
+    return Extension(
+      (b) => b
+        ..name = '${className}AggregateFieldSelectorExtension'
+        ..types.addAll(typeParameters)
+        ..on = targetType
+        ..docs.add(
+          '/// Generated AggregateFieldSelector for $classNameWithTypeParams',
+        )
+        ..methods.addAll(methods),
     );
-    buffer.writeln('');
-  }
-
-  /// Generate nested custom type accessor for aggregate field selector
-  static void _generateNestedCustomTypeAccessor(
-    StringBuffer buffer,
-    FieldInfo field,
-  ) {
-    buffer.writeln(
-      '  /// Access nested ${field.parameterName} aggregate fields',
-    );
-    buffer.writeln(
-      '  AggregateFieldSelector<${field.dartType}> get ${field.parameterName} => AggregateFieldSelector(name: \'${field.jsonFieldName}\', parent: this);',
-    );
-    buffer.writeln('');
   }
 }
