8c61c42f0c2014b5078fe8e44f5c038878ed2749
shtse8 <shtse8@gmail.com>
2025-07-13 00:44:50 +0100

    refactor: remove unused Node class for improved clarity and maintainability

--- packages/firestore_odm/lib/src/field_selecter.dart
+++ packages/firestore_odm/lib/src/field_selecter.dart
@@ -1,30 +1,5 @@
 import 'package:firestore_odm/firestore_odm.dart';
 
-class Node {
-  final String _name;
-  final Node? _parent;
-
-  Node get $root => _parent?.$root ?? this;
-
-  String get $name => _name;
-
-  String get $path {
-    if (_parent == null) return _name;
-    if (_name.isEmpty) return _parent.$path;
-    return _parent.$path.isEmpty ? _name : '${_parent.$path}.$_name';
-  }
-
-  List<String> get $parts {
-    if (_parent == null) {
-      return _name.isEmpty ? const <String>[] : <String>[_name];
-    }
-    if (_name.isEmpty) return _parent.$parts;
-    return [..._parent.$parts, _name];
-  }
-
-  const Node({String name = '', Node? parent}) : _name = name, _parent = parent;
-}
-
 class Node2 {
   const Node2({this.field = const FieldPath.components([])});
 
